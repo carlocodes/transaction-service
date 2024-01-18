5d86baf3959166ebb374ad1c05657fbbdd6ae278
carlocodes <Gian Uy>
2024-01-18 20:16:55 +0800

    initial commit

---
+++ src/test/java/com/carlocodes/transactionservice/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlocodes.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
